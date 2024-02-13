22d085774b98827f378b04b041264d902d855648
CosmeNicolas <nicomas7590@gmail.com>
2024-02-13 16:37:32 -0300

    inicialice el proyecto blog de recetas y lo llame  'cookinando'

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {Container} from 'react-bootstrap'
+import Footer from './components/common/Footer'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+ 
+
+  return (
+    <>
+     <Container className="main" >
+      <h1>Aca inicia el proyecto</h1>
+     </Container>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+  return (
+    <footer className='py-3 text-light text-center'>
+        &copy; Todos los Derechos reservados
+    </footer>
+  )
+}
+
+export default Footer
