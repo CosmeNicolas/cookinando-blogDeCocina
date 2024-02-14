56e1d5859c4e00f9e712b26d8a041823f16bc75d
CosmeNicolas <nicomas7590@gmail.com>
2024-02-14 17:46:28 -0300

    agregue estilos al boton de crear nueva receta

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,7 +1,10 @@
-import { Container, Button } from "react-bootstrap"
+import { Container, Button, Table} from "react-bootstrap"
 import {Link} from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCirclePlus} from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faFolderPlus } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -14,12 +17,42 @@ const Administrador = () => {
       </h1>
       
     <Link to='/administrador/crear'>
-    <Button variant="dark" className="botonColor mt-2">
-    <FontAwesomeIcon icon={faCirclePlus} />
+    <Button variant="dark" className="botonColorCrear mt-2">
+    <FontAwesomeIcon icon={faCirclePlus}  />
     </Button>
     </Link>
       </div>
       <hr/>
+      {/* Tabla */}
+      <Table responsive striped bordered hover variant='dark' size='sm' >
+      <thead>
+        <tr>
+          <th>Cod</th>
+          <th>Producto</th>
+          <th>Precio</th>
+          <th>URLimg</th>
+          <th>Categoría</th>
+          <th>Opciones</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+          <td>1</td>
+          <td>Nombre Receta</td>
+          <td>ingredientes</td>
+          <td>📝</td>
+          <td>Categoria</td>
+          <td className='d-flex py-5  justify-content-center'>
+          <Button variant="warning" className='me-1' >
+            <FontAwesomeIcon icon={faPenToSquare} />
+            </Button>
+            <Button variant="danger" >
+            <FontAwesomeIcon icon={faTrash} className=""/>
+            </Button>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
       </Container>
   )
 }
