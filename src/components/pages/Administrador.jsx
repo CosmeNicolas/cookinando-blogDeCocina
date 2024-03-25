bb8ded4acc69c54d2d8890ba41280df8f80e7a75
CosmeNicolas <nicomas7590@gmail.com>
2024-03-24 22:20:34 -0300

    agregue la funcion borrar receta por su id para recebirlo como parametro en el componente administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -33,6 +33,45 @@ const Administrador = () => {
     }
   };
 
+  const borrarReceta = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        try {
+          const respuesta = await borrarReceta(id);
+          if (respuesta.status === 200) {
+            const productosActualizados = await leerRecetasAPI()
+            setProductos(productosActualizados)
+            traerRecetas();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+
+          } else {
+            console.log("Error al borrar la receta:", respuesta.status);
+            // Puedes manejar otros códigos de estado aquí si es necesario
+          }
+        } catch (error) {
+          console.log("Error en la solicitud DELETE:", error);
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success",
+          });
+        }
+      }
+    });
+  };
+
 
 
   return (
